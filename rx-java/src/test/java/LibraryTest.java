b05bfa5119321eadefcc312636d5495d9da2f45a
Luis Trigueiros <luistrigueiros@gmail.com>
2016-06-26 17:46:31 +0100

    saving work

--- rx-java/src/test/java/LibraryTest.java
+++ rx-java/src/test/java/LibraryTest.java
@@ -16,11 +16,14 @@ public class LibraryTest {
     }
 
     @Test
-    public void testFlatMap() {
+    public void testComposition1() {
         Observable
                 .just(8, 9, 10)
+                .doOnNext(i->System.out.println("A:" + i))
                 .filter(i -> i % 3 > 0)
+                .doOnNext(i->System.out.println("B:" + i))
                 .map(i -> "#" + i * 10)
+                .doOnNext(i->System.out.println("C:" + i))
                 .filter(s -> s.length() < 4)
                 .subscribe(System.out::println);
     }
